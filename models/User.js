c9119aaaf8d5a0ffd711943592f914a0a3b5d29f
leedakoog <leedakoog@naver.com>
2022-05-29 16:47:34 +0900

    haha

--- models/User.js
+++ models/User.js
@@ -8,8 +8,8 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        trim: true,  //공백을 없애주는 역할
-        unique: 1
+        trim: true,    //공백을 없애주는 역할
+        unique: 1      //같은 이메일은 사용하지 못하게
     },
     password: {
         type: String,
@@ -19,19 +19,19 @@ const userSchema = mongoose.Schema({
         type: String,
         maxlength: 50
     },
-    role: {
+    role: {            //관리자 or 유저
         type: Number,
         default: 0
     },
     Image: String,
-    token: {
+    token: {           //유효성 관리
         type: Stirng
     },
-    tokenExp: {
+    tokenExp: {        //토큰을 사용할 수 있는 기간 관리
         type: Number
     }
 })
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema) //스키마를 모델로 감싸줌
 
-module.exports = {User}
+module.exports = {User} //다른 파일에서도 사용가능하게
